16ca19e34e777cf1a640c1d64a475503535ada48
Peter Pak <ppak10@gmail.com>
2021-05-22 19:25:47 -0500

    Outlines search and download functionality.

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+/**
+ * _app.tsx
+ * App component.
+ */
+
+// Node Modules
+import type {AppProps} from 'next/app';
+
+export default function App({Component}: AppProps) {
+  return (
+    <Component />
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,64 @@
  * Home page component.
  */
 
+// Node Modules
+import {useState} from 'react';
+
+// Constants
+const DISCOVERY_API_ROUTE = 'https://discovery-b.mainnet.audius.radar.tech/v1/full/search/autocomplete';
+const NODE_API_ROUTE = 'https://creatornode3.audius.co/ipfs';
+
 export default function Home() {
+  // Hooks
+  const [request, setRequest] = useState('');
+  const [data, setData] = useState({});
+
+  // Callbacks
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${DISCOVERY_API_ROUTE}?limit=3&offset=0&query=${request}`);
+    const data = await response.json();
+    console.log('data', data)
+    if (response.status === 200) {
+      setData(data.data);
+    }
+  };
+
+  const handleDownload = async (cid) => {
+    if (cid) {
+      const response = await fetch(`${NODE_API_ROUTE}/${cid}`);
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `${cid}.mp3`;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }
+  };
+
+  // JSX
+  const trackListItemsJSX = data.tracks && data.tracks.map((track) => (
+    <li key={track.id}>
+      <h1>{track.title}</h1>
+      <p>{track.description}</p>
+      <button onClick={() => handleDownload(track.download.cid)}>Download</button>
+    </li>
+  ));
+
   return (
     <div>
-      Hello World
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Enter Search:</legend>
+          <input value={request} onChange={(e) => setRequest(e.target.value)} />
+        </fieldset>
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {trackListItemsJSX}
+      </ul>
     </div>
   );
 }
